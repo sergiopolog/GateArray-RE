af44e6bc546efa3f27f5afa9fdb19783a1c7d131
sergiopolog <sergiopolog@gmail.com>
2024-07-12 11:22:37 +0200

    Added: NMK/nmk-111

---
+++ NMK/nmk-111/README.md
@@ -0,0 +1,12 @@
+# NMK-111
+
+NEC CMOS-4(?) gate array. Model 65006 in a QFP64 package
+
+16-bit bus handling logic. Tipically used for latching RGB output stage (previous to DAC) and handling palette RAM. Also used for handling BG VRAM.
+
+SVG file could be overlapped on die image from: [https://siliconpr0n.org/archive/doku.php?id=furrtek:nmk:111](https://siliconpr0n.org/archive/doku.php?id=furrtek:nmk:111)
+
+
+Huge thanks to:
+* [@furrtek](https://github.com/furrtek) for decapping and imaging the silicon die
+* [Caius](https://x.com/caiusarcade) for donating the chip
---
+++ NMK/nmk-111/nmk-111.v
@@ -0,0 +1,47 @@
+// NMK-111 logic def
+// Sergio Galiano 'Recreativos Piscis' @Recre_Piscis
+// GPLV3 - See LICENSE
+
+// Untested !
+
+module NMK_111(
+	input CLK1,
+	input CLK2,
+	input MODE,
+	input RST,
+	input nCS,
+	input DIR,
+	inout [15:0] ABUS,
+	inout [15:0] BBUS,
+	output [15:0] OBUS
+);
+
+reg [15:0] REG_1;
+reg [11:0] REG_2;
+
+
+assign ABUS = !nCS &  DIR ? BBUS : 16'bZ;	// DIR = 1:  B --> A
+assign BBUS = !nCS & !DIR ? ABUS : 16'bZ;	// DIR = 0:  A --> B
+
+
+always @(posedge CLK1 or posedge RST)
+begin
+	if (RST == 1'b1)
+		REG_1 <= 16'b0;
+	else
+		REG_1 <= ABUS;
+end
+
+always @(posedge CLK2 or posedge RST)
+begin
+	if (RST == 1'b1)
+		REG_2 <= 12'b0;
+	else
+		REG_2 <= REG_1[11:0];
+end
+
+
+assign OBUS = MODE ? REG_1 : {REG_1[15:12], REG_2};
+
+		
+endmodule
